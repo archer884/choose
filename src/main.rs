29c72a610126e2b5248b448c06d08ca06070e13a
J/A <archer884@zoho.com>
2021-04-17 15:13:52 -0500

    Add shuffle mode

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
 use std::io::{self, Read};
 
-use clap::Clap;
 use rand::prelude::SliceRandom;
+use structopt::StructOpt;
 
-#[derive(Clap, Clone, Debug)]
+#[derive(StructOpt, Clone, Debug)]
 struct Opts {
     count: Option<usize>,
+    #[structopt(short, long)]
+    shuffle: bool,
 }
 
 fn main() -> io::Result<()> {
-    let opts = Opts::parse();
+    let opts = Opts::from_args();
     let content = read_stdin()?;
-    let choices: Vec<_> = content.lines().collect();
+    let mut choices: Vec<_> = content.lines().collect();
 
-    for &item in choices.choose_multiple(&mut rand::thread_rng(), opts.count.unwrap_or(1)) {
-        println!("{}", item);
+    if opts.shuffle {
+        choices.shuffle(&mut rand::thread_rng());
+        for item in choices {
+            println!("{}", item);
+        }
+    } else {
+        for &item in choices.choose_multiple(&mut rand::thread_rng(), opts.count.unwrap_or(1)) {
+            println!("{}", item);
+        }
     }
 
     Ok(())
